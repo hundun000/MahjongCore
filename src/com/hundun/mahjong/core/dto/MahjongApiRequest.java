f1ff1a9f6ccc36d6e43b592f6f062e09fd732381
hundun <wwj.hddjj@gmail.com>
2021-03-31 00:08:49 +0800

    add dto

---
+++ src/com/hundun/mahjong/core/dto/MahjongApiRequest.java
@@ -0,0 +1,11 @@
+package com.hundun.mahjong.core.dto;
+/**
+ * @author hundun
+ * Created on 2021/03/31
+ */
+public class MahjongApiRequest {
+
+    public String mode;
+    public String board_id;
+
+}
